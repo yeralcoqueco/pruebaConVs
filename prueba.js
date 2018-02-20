dd638ad1af04ec5a4ef17e2bc667f4feb6ef2c76
yeralcoqueco <YERALCOQUECO@HMAIL.COM>
2018-02-20 12:46:08 -0500

    información CCPU, cambiando todo el texto para prueba

--- prueba.js
+++ prueba.js
@@ -1,1 +1,21 @@
-console.log("Hello world");
+const os = require('os');
+const fs = require('fs');
+const mm = require('./prueba.js')
+
+let cpu = os.cpus();
+let sistema = os.platform();
+let usuario = os.hostname();
+
+/*let cpu_string = JSON.stringify(cpu);
+
+fs.appendFile('informacion.txt', `Información del cpu: ${cpu_string}` , function(error){
+    if(error){
+        console.log('Error a crear el archivo');
+    }
+});*/
+
+/*console.log(cpu);
+console.log(sistema);
+console.log(usuario);*/
+
+mm.saludar;
